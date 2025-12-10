5a5f42417dd45306a80221ebd37d2a4e5e740f73
GrandhiArun1234 <arungrandhi.arahinfotech@gmail.com>
2025-12-10 23:18:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://nizam-college-abids.netlify.app",
+      "https://chaitanyadeemedcollege.netlify.app",
       "http://localhost:3000"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
